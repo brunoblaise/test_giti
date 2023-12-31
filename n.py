b3b5b8ab5ffc7b8e7880e83a4f032ee80ee09f3d
brunoblaise94 <brunoblaise94@gmail.com>
2024-01-01 00:34:16 +0200

    commit

--- n.py
+++ n.py
@@ -1,11 +1,14 @@
 import os
+import datetime
 from random import randint
-
+date = datetime.datetime.now()
 for i in range(1, 10065):
     for j in range(0, randint(1, 20)):
-     d = str(i) + ' days ago'
+     d =f'{str(i)} days ago \n'
      with open('file.txt', 'a') as File:
             File.write(d)
-    os.system('git commit -am "commit" ')
+    os.system('git add .')
+    os.system(f'git commit -m "commit" ')
 
+#os.system('git add .')
 os.system('git push -u origin main')
